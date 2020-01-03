fafd4bd49723c123acf3d0d4ea5ad44eb7467a4d
joohyuk <vfrvfr4207@gmail.com>
2020-01-03 17:51:26 +0900

    index.mustache 생성 및 테스트

---
+++ src/test/java/com/weekbelt/firstboard/web/IndexControllerTest.java
@@ -0,0 +1,28 @@
+package com.weekbelt.firstboard.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+public class IndexControllerTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void 메인페이지_로딩() throws Exception {
+        //given
+        String body = this.restTemplate.getForObject("/", String.class);
+
+        //then
+        assertThat(body).contains("SpringBoot와 JPA를 활용한 웹 서비스");
+    }
+}
