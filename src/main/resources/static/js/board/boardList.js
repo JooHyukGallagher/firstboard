b22a43169351f1a96dda61c11e7b28be7836b094
joohyuk <vfrvfr4207@gmail.com>
2020-01-14 18:14:52 +0900

    카테고리별 게시글 리스트 조회 화면

--- src/main/resources/static/js/board/boardList.js
+++ src/main/resources/static/js/board/boardList.js
@@ -1,38 +1,50 @@
 const boardList = {
-    init: async function () {
-        let tbody = document.querySelector("#tbody");
-        let boarListResponseList = await getData("/api/board/list?boardType=ALL");
-        boarListResponseList.forEach(boardListResponse => {
-            const data = {
-                id: boardListResponse.id,
-                title: boardListResponse.title,
-                replyCount: boardListResponse.replyCount,
-                boardType: boardListResponse.boardType,
-                nickname: boardListResponse.nickname,
-                viewCount: boardListResponse.viewCount,
-                createDate: boardListResponse.createDate
-            };
+    init: function () {
+        let prevButton = document.querySelector("#ALL");
+        let currentButton;
 
+        const categoryList = document.querySelector("#category");
+        categoryList.addEventListener("click", (evt) => {
+            // 클릭시 이전 카테고리 active 삭제 및 해당 카테고리 active 표시
+            prevButton.classList.remove("active");
+            currentButton = evt.target;
+            currentButton.classList.add("active");
+            prevButton = evt.target;
+            this.requestBoardList(evt);
+        })
+    },
+    requestBoardList: async function (evt) {
+        const categoryButton = evt.target;
+        if (categoryButton.classList.contains("nav-link")) {
+            const boarListResponseList = await getData("/api/board/list?boardType=" + categoryButton.id);
+            boardList.setBindTemplate(boarListResponseList);
+        }
+    },
+    setBindTemplate: function (boardListResponseList) {
+        // 기존 리스트 삭제
+        const boardListContainer = document.querySelector("#tbody");
+        boardListContainer.innerHTML = "";
+
+        boardListResponseList.forEach(boardListResponse => {
+            const data = boardList.createBindingData(boardListResponse);
             const boardListTemplate = document.querySelector("#boardListTemplate").innerHTML;
 
             const bindTemplate = Handlebars.compile(boardListTemplate);
             const result = bindTemplate(data);
-            tbody.innerHTML += result;
+            boardListContainer.innerHTML += result;
         });
-
-        const categoryList = document.querySelector("#category");
-        categoryList.addEventListener("click", (evt) => {
-            this.requestBoardList(evt);
-        })
     },
-    // requestBoardList: async function (evt) {
-    //         const categoryButton = evt.target;
-    //         if(categoryButton.classList.contains("nav-link")){
-    //             const data = await getData("/api/board/list?boardType=" + categoryButton.id);
-    //             const template = document.querySelector("#boardListTemplate").innerHTML;
-    //             console.log(template);
-    //         }
-    // }
+    createBindingData: function (boardListResponse) {
+        return {
+            id: boardListResponse.id,
+            title: boardListResponse.title,
+            replyCount: boardListResponse.replyCount,
+            boardType: boardListResponse.boardType,
+            nickname: boardListResponse.nickname,
+            viewCount: boardListResponse.viewCount,
+            createDate: boardListResponse.createDate
+        };
+    }
 };
 
 document.addEventListener("DOMContentLoaded", () => {
